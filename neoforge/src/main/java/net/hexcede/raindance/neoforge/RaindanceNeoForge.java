1ac55e4655c489bf5e7dd2796570109a99f20f24
Hexcede <8394472+Hexcede@users.noreply.github.com>
2024-11-11 19:14:06 -0500

    Minecraft 1.21

---
+++ neoforge/src/main/java/net/hexcede/raindance/neoforge/RaindanceNeoForge.java
@@ -0,0 +1,11 @@
+package net.hexcede.raindance.neoforge;
+
+import net.hexcede.raindance.Raindance;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Raindance.MOD_ID)
+public class RaindanceNeoForge {
+    public RaindanceNeoForge() {
+        Raindance.init();
+    }
+}
