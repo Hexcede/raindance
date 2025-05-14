17f9e4dae26f726baf0ec71c446903a294ee37a6
Hexcede <8394472+Hexcede@users.noreply.github.com>
2025-05-13 20:03:44 -0400

    Neoforge 1.20.4 config screen

--- neoforge/src/main/java/net/hexcede/raindance/neoforge/RaindanceNeoForge.java
+++ neoforge/src/main/java/net/hexcede/raindance/neoforge/RaindanceNeoForge.java
@@ -1,21 +1,28 @@
 package net.hexcede.raindance.neoforge;
 
+import java.util.function.BiFunction;
+
 import net.hexcede.raindance.Raindance;
 import net.hexcede.raindance.config.RaindanceConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 @Mod(Raindance.MOD_ID)
 public class RaindanceNeoForge {
+    public record ConfigScreen(BiFunction<Minecraft, Screen, Screen> screenFunction) implements IExtensionPoint<ConfigScreen> {}
+
     public RaindanceNeoForge() {
         Raindance.init();
 
         ModLoadingContext.get().registerExtensionPoint(
-            IConfigScreenFactory.class,
-            () -> (client, parent) -> RaindanceConfig.HANDLER.instance()
-                .buildGui()
-                .generateScreen(parent)
+            ConfigScreenHandler.ConfigScreenFactory.class,
+            () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> RaindanceConfig.HANDLER.instance()
+                    .buildGui()
+                    .generateScreen(parent))
         );
     }
 }
